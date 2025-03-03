de1244ec6368dccfe44faa5f7f58c3d03c223117
radekaadek <radekaadek@gmail.com>
2025-03-03 17:45:31 +0100

    more

--- app/src/main/java/pl/pw/graterenowa/MainActivity.kt
+++ app/src/main/java/pl/pw/graterenowa/MainActivity.kt
@@ -7,8 +7,35 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.util.Log
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+                Toast.makeText(
+                    this,
+                    "Permission granted",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // feature requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("MainActivity", "onCreate")
@@ -24,6 +51,9 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         Log.d("MainActivity", "onStart")
+        findViewById<Button>(R.id.map_button).setOnClickListener {
+            Log.d("MainActivity", "CLICK!!")
+        }
     }
 
     override fun onPause() {
@@ -34,9 +64,7 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         Log.d("MainActivity", "onResume")
-        var button = findViewById<Button>(R.id.map_button).setOnClickListener {
-            Log.d("MainActivity", "CLICK!!")
-        }
+        checkLocationPermission()
     }
 
     override fun onStop() {
@@ -54,4 +82,44 @@ class MainActivity : AppCompatActivity() {
         Log.d("MainActivity", "onRestart")
     }
 
+    private fun checkLocationPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                    Toast.makeText(
+                        this,
+                        "Location permission is required for scanning beacons",
+                        Toast.LENGTH_LONG
+                    ).show()
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
+    private fun showLocationPermissionExplanation() {
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("Location Permission")
+            .setMessage("This app needs the Location permission in order to scan beacons.")
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+        alertDialog.show()
+    }
+
+    private fun startBeaconScanning() {
+        // TODO beacon scanning
+        Log.d("MainActivity", "Permission granted, scanning beacons...")
+    }
 }
