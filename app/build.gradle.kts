c5dc751267e192c84374a8a5c0ea7f609f4a0539
radekaadek <radekaadek@gmail.com>
2025-03-03 16:53:16 +0100

    beacon lib added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.google.play.services.location)
+    implementation(libs.android.beacon.library)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
