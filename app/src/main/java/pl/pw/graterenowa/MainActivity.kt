b57d0daf1b8d4ebe70b70edd5e634025af9acc98
radekaadek <radekaadek@gmail.com>
2025-03-04 09:23:05 +0100

    permission checking

--- app/src/main/java/pl/pw/graterenowa/MainActivity.kt
+++ app/src/main/java/pl/pw/graterenowa/MainActivity.kt
@@ -36,6 +36,9 @@ class MainActivity : AppCompatActivity() {
                 // decision.
             }
         }
+
+    private var permissionsGranted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("MainActivity", "onCreate")
@@ -64,7 +67,12 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         Log.d("MainActivity", "onResume")
-        checkLocationPermission()
+        checkPermissions()
+        if (permissionsGranted) {
+            startBeaconScanning()
+        } else {
+            showPermissionsExplanation()
+        }
     }
 
     override fun onStop() {
@@ -82,35 +90,70 @@ class MainActivity : AppCompatActivity() {
         Log.d("MainActivity", "onRestart")
     }
 
-    private fun checkLocationPermission() {
+    private fun checkPermissions(): Boolean {
+        permissionsGranted = (checkLocationPermission() && checkBluetoothPermission())
+        return permissionsGranted
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        return checkPermission(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            "Location permission is required for scanning beacons"
+        )
+    }
+
+    private fun checkBluetoothPermission(): Boolean {
+        return checkPermission(
+            Manifest.permission.BLUETOOTH_SCAN,
+            "Bluetooth scanning permission is required for scanning beacons"
+        ) && checkPermission(
+            Manifest.permission.BLUETOOTH_CONNECT,
+            "Bluetooth connecting permission is required for scanning beacons"
+        )
+    }
+
+    private fun checkPermission(permission: String, rationale: String): Boolean {
         when {
             ContextCompat.checkSelfPermission(
                 this,
-                Manifest.permission.ACCESS_FINE_LOCATION
+                permission
             ) == PackageManager.PERMISSION_GRANTED -> {
-
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+                return true
             }
             ActivityCompat.shouldShowRequestPermissionRationale(
-                this, Manifest.permission.ACCESS_FINE_LOCATION) -> {
-                    Toast.makeText(
-                        this,
-                        "Location permission is required for scanning beacons",
-                        Toast.LENGTH_LONG
-                    ).show()
+                this,
+                permission
+            ) -> {
+                // Explain to the user that the feature is unavailable because the
+                // feature requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+                Toast.makeText(
+                    this,
+                    rationale,
+                    Toast.LENGTH_LONG
+                ).show()
+                return false
             }
             else -> {
                 // You can directly ask for the permission.
                 // The registered ActivityResultCallback gets the result of this request.
-                requestPermissionLauncher.launch(
-                    Manifest.permission.ACCESS_FINE_LOCATION)
+                requestPermissionLauncher.launch(permission)
+                return ActivityCompat.checkSelfPermission(
+                        this,
+                        permission
+                    ) == PackageManager.PERMISSION_GRANTED
             }
         }
     }
 
-    private fun showLocationPermissionExplanation() {
+    private fun showPermissionsExplanation() {
         val alertDialog = AlertDialog.Builder(this)
             .setTitle("Location Permission")
-            .setMessage("This app needs the Location permission in order to scan beacons.")
+            .setMessage("This app needs the Location and Bluetooth permission in order to scan beacons.")
             .setPositiveButton("OK") { dialog, _ ->
                 dialog.dismiss()
             }
@@ -120,6 +163,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun startBeaconScanning() {
         // TODO beacon scanning
-        Log.d("MainActivity", "Permission granted, scanning beacons...")
+        Log.d("MainActivity", "Permissions granted, scanning beacons...")
     }
 }
