4d4e7c4470a10df2b05de3f78624ce821e8d4620
radekaadek <radekaadek@gmail.com>
2025-05-26 15:49:12 +0200

    esri

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
     implementation(libs.osmdroid)
     implementation(libs.gson)
+    implementation(libs.arcgis.maps.kotlin)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
